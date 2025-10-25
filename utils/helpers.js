54bee9ea34342b5967b559cdb67b7ab281cc0b9a
Raghad <raghadjamhour@gmail.com>
2025-10-25 22:24:54 +0200

    first

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,9 +2,20 @@
 import fs from 'fs'
 
 export function readJSON(filePath) {
-  return JSON.parse(fs.readFileSync(filePath))
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8')
+    return JSON.parse(data)
+  } catch (e) {
+    console.error('Error reading JSON:', e)
+    return { books: [], reviews: [] } // default fallback
+  }
 }
 
 export function writeJSON(filePath, data) {
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+  try {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+  } catch (e) {
+    console.error('Error writing JSON:', e)
+    throw e
+  }
 }
